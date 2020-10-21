c65e9b4aa14416b1ef3afc249945c1246fe84581
Abhishek Taparia <abhishek@wizpanda.com>
2020-10-21 16:33:14 +0530

    Permission Prommpt Added

--- www/CooeeSdkPlugin.js
+++ www/CooeeSdkPlugin.js
@@ -1,25 +1,54 @@
 var exec = require("cordova/exec");
 
 exports.coolMethod = function (arg0, success, error) {
-  exec(success, error, "CooeeSdkPlugin", "coolMethod", [arg0]);
+    exec(success, error, "CooeeSdkPlugin", "coolMethod", [arg0]);
 };
 
 exports.init = function (success, error) {
-  exec(success, error, "CooeeSdkPlugin", "init", []);
+    checkPermessions();
+    exec(success, error, "CooeeSdkPlugin", "init", []);
 };
 
 exports.sendEvent = function (arg0, arg1, success, error) {
-  exec(success, error, "CooeeSdkPlugin", "sendEvent", [arg0, arg1]);
+    exec(success, error, "CooeeSdkPlugin", "sendEvent", [arg0, arg1]);
 };
 
 exports.updateUserData = function (arg0, success, error) {
-  exec(success, error, "CooeeSdkPlugin", "updateUserData", [arg0]);
+    exec(success, error, "CooeeSdkPlugin", "updateUserData", [arg0]);
 };
 
 exports.updateUserProperties = function (arg0, success, error) {
-  exec(success, error, "CooeeSdkPlugin", "updateUserProperties", [arg0]);
+    exec(success, error, "CooeeSdkPlugin", "updateUserProperties", [arg0]);
 };
 
 exports.updateUserProfile = function (arg0, arg1, success, error) {
-  exec(success, error, "CooeeSdkPlugin", "updateUserProfile", [arg0, arg1]);
+    exec(success, error, "CooeeSdkPlugin", "updateUserProfile", [arg0, arg1]);
+};
+
+checkPermessions = function () {
+    var permissions = cordova.plugins.permissions;
+    var perm = [
+        "android.permission.ACCESS_COARSE_LOCATION",
+        "android.permission.ACCESS_FINE_LOCATION",
+    ];
+
+    permissions.checkPermission(
+        "android.permission.ACCESS_COARSE_LOCATION",
+        function (status) {
+            if (!status.hasPermission) {
+                permissions.requestPermissions(
+                    perm,
+                    function (status) {
+                        console.log("Permission Granted");
+                    },
+                    function (err) {
+                        console.log("Permission Denied");
+                    }
+                );
+            }
+        },
+        function (err) {
+            console.log(err);
+        }
+    );
 };
