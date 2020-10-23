3aef7620db4b246e8415f6ae42330001e21ed0a7
Shashank Agrawal <shashank@wizpanda.com>
2020-10-23 18:40:31 +0530

    SDK should not ask for the permissions.

--- www/CooeeSdkPlugin.js
+++ www/CooeeSdkPlugin.js
@@ -2,7 +2,6 @@ const exec = require("cordova/exec");
 
 // Initialize the Cooee instance
 exports.init = function (success, error) {
-    checkPermissions();
     exec(success, error, "CooeeSdkPlugin", "init", []);
 };
 
@@ -24,33 +23,4 @@ exports.updateUserProperties = function (properties, success, error) {
 // Send user data and user properties to the server
 exports.updateUserProfile = function (arg0, arg1, success, error) {
     exec(success, error, "CooeeSdkPlugin", "updateUserProfile", [arg0, arg1]);
-};
-
-// Check if the application has required permissions
-checkPermissions = function () {
-    const permissionsPlugin = cordova.plugins.permissions;
-    const perm = [
-        "android.permission.ACCESS_COARSE_LOCATION",
-        "android.permission.ACCESS_FINE_LOCATION"
-    ];
-
-    permissionsPlugin.checkPermission(
-        "android.permission.ACCESS_COARSE_LOCATION",
-        function (status) {
-            if (!status.hasPermission) {
-                permissionsPlugin.requestPermissions(
-                    perm,
-                    function (status) {
-                        console.log("Permission Granted");
-                    },
-                    function (err) {
-                        console.log("Permission Denied");
-                    }
-                );
-            }
-        },
-        function (err) {
-            console.log(err);
-        }
-    );
 };
