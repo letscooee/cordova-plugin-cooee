c0575ef3de316f578c4dc2b777b912a0ba31bff5
Ashish Gaikwad <ashish@letscooee.com>
2022-03-02 15:30:46 +0530

    refactor 📦: Revert version bump for plugin.xml COOEE-606
    As replacement pattern is different in plugin.xml and Constant files; Plugin version will be updated with old code itself. And new function will bump version in constant

--- bump-version.js
+++ bump-version.js
@@ -3,7 +3,10 @@
 const fs = require('fs');
 const version = require('./package.json').version;
 
-bumpVersion('./plugin.xml')
+let data = fs.readFileSync('./plugin.xml', "utf8");
+data = data.replace(/version="[^"]+"/, `version="${version}"`);
+fs.writeFileSync('./plugin.xml', data);
+
 bumpVersion('./src/android/Constants.java')
 bumpVersion('./src/ios/Constants.swift')
 
