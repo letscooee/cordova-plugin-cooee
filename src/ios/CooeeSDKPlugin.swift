f7b738d1eca77bc51e92d382f3825db2636194c4
Ashish Gaikwad <ashish@letscooee.com>
2022-03-02 15:42:17 +0530

    refactor 📦: Format swift file COOEE-606

--- src/ios/CooeeSDKPlugin.swift
+++ src/ios/CooeeSDKPlugin.swift
@@ -8,41 +8,41 @@ class CooeeSDKPlugin: CDVPlugin {
     override func pluginInitialize() {
         super.pluginInitialize()
         self.commandDelegate.run(inBackground: {
-	        AppController.configure()
+            AppController.configure()
             self.cooeeSDK = CooeeSDK.getInstance()
             self.cooeeSDK?.setWrapper("cordova")
-		})
+        })
     }
 
     @objc(sendEvent:)
     func sendEvent(command: CDVInvokedUrlCommand) {
-	    self.commandDelegate.run(inBackground: {
-	        let eventName = command.argument(at: 0) as! String
-	        let eventProperties = command.argument(at: 1) as? [String: Any]
-	        do {
-	            if let eventProperties = eventProperties {
-	                try self.cooeeSDK?.sendEvent(eventName: eventName, eventProperties: eventProperties)
-	            } else {
-	                try self.cooeeSDK?.sendEvent(eventName: eventName)
-	            }
-	            self.commandDelegate.send(CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "Event sent"), callbackId: command.callbackId)
-	        } catch {
-	            self.commandDelegate.send(CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: [error]), callbackId: command.callbackId)
-	        }
-		})
+        self.commandDelegate.run(inBackground: {
+            let eventName = command.argument(at: 0) as! String
+            let eventProperties = command.argument(at: 1) as? [String: Any]
+            do {
+                if let eventProperties = eventProperties {
+                    try self.cooeeSDK?.sendEvent(eventName: eventName, eventProperties: eventProperties)
+                } else {
+                    try self.cooeeSDK?.sendEvent(eventName: eventName)
+                }
+                self.commandDelegate.send(CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "Event sent"), callbackId: command.callbackId)
+            } catch {
+                self.commandDelegate.send(CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: [error]), callbackId: command.callbackId)
+            }
+        })
     }
 
     @objc(updateUserProfile:)
     func updateUserProfile(command: CDVInvokedUrlCommand) {
-	    self.commandDelegate.run(inBackground: {
-	        let userData = command.argument(at: 0) as! [String: Any]
-	        do {
-	            try self.cooeeSDK?.updateUserProfile(userData)
-	            self.commandDelegate.send(CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "User profile updated"), callbackId: command.callbackId)
-	        } catch {
-	            self.commandDelegate.send(CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Fail to update user profile: \(error)"), callbackId: command.callbackId)
-	        }
-		})
+        self.commandDelegate.run(inBackground: {
+            let userData = command.argument(at: 0) as! [String: Any]
+            do {
+                try self.cooeeSDK?.updateUserProfile(userData)
+                self.commandDelegate.send(CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "User profile updated"), callbackId: command.callbackId)
+            } catch {
+                self.commandDelegate.send(CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Fail to update user profile: \(error)"), callbackId: command.callbackId)
+            }
+        })
     }
 
     @objc(setCurrentScreen:)
