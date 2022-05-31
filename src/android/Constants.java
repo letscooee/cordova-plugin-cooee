375193f5dcbd6fb830cec1c80132d7d9f129e5ac
Ashish Gaikwad <ashish@letscooee.com>
2022-05-31 08:40:00 +0530

    release: 1.3.7

--- src/android/Constants.java
+++ src/android/Constants.java
@@ -10,7 +10,7 @@ public class Constants {
 
     // CORDOVA_PLUGIN_VERSION will be bumped via commandline while publishing the plugin
     // i.e. when npm version is executed
-    public static String CORDOVA_PLUGIN_VERSION = "1.3.6";
+    public static String CORDOVA_PLUGIN_VERSION = "1.3.7";
     public static final String FCM_MESSAGE_ACTION = "by.chemerisuk.cordova.firebase.ACTION_FCM_MESSAGE";
     public static final String FCM_MESSAGE_DATA = "by.chemerisuk.cordova.firebase.EXTRA_FCM_MESSAGE";
 }
