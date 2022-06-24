14441c40ef339fd60f4ce3b9566f62534f331745
Ashish Gaikwad <ashish@letscooee.com>
2022-06-24 16:22:42 +0530

    refactor: Break single method in two COOEE-940
    For more code readability break one method in different methods.

--- src/ios/hooks/install_notification_service.js
+++ src/ios/hooks/install_notification_service.js
@@ -21,42 +21,38 @@ module.exports = function (context) {
     const argumentArray = process.argv;
 
     /**
-     * Find and access COOEE_APP_ID at process.argv and package.json and returns it.
-     * If not found, Throws error.
+     * Finds COOEE_APP_ID at process.argv.
+     * Works while adding plugin to project.
+     *
      * @returns {string|*}
      */
-    function getAppID() {
-        let appId = "";
-        /**
-         * Finds COOEE_APP_ID at process.argv.
-         * Works while adding plugin to project.
-         */
+    function getAppIDFromCMD() {
         argumentArray.forEach(function (arg) {
             if (arg.startsWith("COOEE_APP_ID")) {
-                appId = arg.split("=")[1];
+                return arg.split("=")[1];
             }
         });
 
-        if (appId && appId !== "") {
-            return appId;
-        }
+        return null;
+    }
 
-        /**
-         * Finds COOEE_APP_ID at package.json.
-         * Works when plugin is installed and platform is getting add.
-         */
+    /**
+     * Finds COOEE_APP_ID at package.json.
+     * Works when plugin is installed and platform is getting add.
+     *
+     * @returns {string|*}
+     */
+    function getAppIDFromPackageJSON() {
         let json = JSON.parse(fs.readFileSync('package.json', 'utf8'))
-        appId = json.cordova.plugins['@letscooee/cordova-plugin'].COOEE_APP_ID;
-
-        if (appId && appId !== "") {
-            return appId;
-        }
-
-        throw new cordovaCommon.CordovaError('Fail to find COOEE_APP_ID in process.argv or package.json. Stopping the build.');
+        return json.cordova.plugins['@letscooee/cordova-plugin'].COOEE_APP_ID;
     }
 
     /******************* Fetch COOEE_APP_ID *******************/
-    let appId = getAppID();
+    let appId = getAppIDFromCMD() ?? getAppIDFromPackageJSON();
+    if (!appId) {
+        throw new cordovaCommon.CordovaError('Fail to find COOEE_APP_ID in process.argv or package.json. Stopping the build.');
+    }
+
     console.log('Found COOEE_APP_ID: ' + appId);
 
     console.log(`Adding ${extName} notification extension to ${appName}`);
