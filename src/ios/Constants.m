b07004770976bbf28e1793d91b9ace75f4e48219
Ashish Gaikwad <ashish@letscooee.com>
2022-09-13 15:23:23 +0530

    refactor: Add version constant in constants files COOEE-1030
    1. Add VERSION_NAME and VERSION_CODE in Constant.java
    2. Remove Constants.swift and added Constant in Objective-C, Because accessing swift file from objective-c making trouble.
    3. Update plugin.xml to handle iOS Constants.
    4. Update bump-version.js to update version in constant files.

---
+++ src/ios/Constants.m
@@ -0,0 +1,17 @@
+//
+//  Constants.m
+//  CooeeReactNative
+//
+//  Created by Ashish Gaikwad on 06/09/22.
+//  Copyright © 2022 LetsCooee. All rights reserved.
+//
+
+#import "Constants.h"
+#import <Foundation/Foundation.h>
+
+@implementation Constants
+
+int VERSION_CODE = 10400;
+NSString *VERSION_NAME = @"1.4.0";
+
+@end
